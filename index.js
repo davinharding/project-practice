0397e672b044c5d1515abeae1953d1b69400580d
Davin Harding <davinlharding@gmail.com>
2018-08-30 14:44:52 -0400

    Created Person class

---
+++ index.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor (levelSuck, levelChill) {
+        this.levelSuck = levelSuck;
+        this.levelChill - levelChill;
+
+    }
+    displayLevels() {
+        console.log(this.levelSuck);
+        console.log(this.levelChill);
+    }
+}
+
+Pio = new Person(100, 0);
+
+console.log(Pio.displayLevels());
