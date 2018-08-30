78b91c9493e0924a61d2aca61e1ec1800b465166
Pio Molina <vpio@comcast.net>
2018-08-30 14:44:49 -0400

    added some js function

---
+++ index.js
@@ -0,0 +1,16 @@
+
+class Davin {
+  constructor(name, money = 0){
+    this.name = name;
+    this.money = money;
+  }
+
+  moneyStatus(){
+    console.log(`I, ${this.name}, am poor.`)
+  }
+}
+
+
+let teammateDavin = new Davin('Davin', 0)
+
+teammateDavin.moneyStatus()
